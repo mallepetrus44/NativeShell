e43e3bc8db4f95bb2e583bfaac46641350d74465
mallepetrus44 <mathijsvandekerkhof@gmail.com>
2024-03-27 09:31:10 +0100

    Added mfe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {loadRemoteModule} from "@angular-architects/native-federation";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'agenda',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Module'
+      })
+        .then(m => m.SaAgendaModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
